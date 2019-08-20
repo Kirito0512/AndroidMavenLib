5d3c39134b8fba65a536a40344c59320e83acca8
xuqi <xuqi@changba.com>
2019-08-20 17:43:46 +0800

    添加对bintray的仓库的引用

--- app/src/main/java/com/example/androidmavenlib/MainActivity.java
+++ app/src/main/java/com/example/androidmavenlib/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.androidmavenlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.androidutillib.AndroidUtil;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity_lib";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "message = " + AndroidUtil.getMessage());
+        Log.d(TAG, "new message = " + AndroidUtil.getNewMessage());
     }
 }
