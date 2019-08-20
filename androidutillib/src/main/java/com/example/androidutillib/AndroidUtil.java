89c2cc55964c432543eb9921d87c83adbf704496
xuqi <xuqi@changba.com>
2019-08-20 17:24:40 +0800

    add 添加lib相关代码

---
+++ androidutillib/src/main/java/com/example/androidutillib/AndroidUtil.java
@@ -0,0 +1,10 @@
+package com.example.androidutillib;
+
+/**
+ * Created by xuqi on 2019-08-20
+ */
+public class AndroidUtil {
+    public static String getMessage() {
+        return "message";
+    }
+}
